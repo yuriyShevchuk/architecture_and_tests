1e8dc5b31c57b491eff2fce5ffc79a61f7f4aa9e
yuriyShevchuk <yura_shevchuk@tut.by>
2022-12-14 16:52:58 +0300

    feat: solved test for zero parameter A

---
+++ const.py
@@ -0,0 +1,4 @@
+from typing import Union
+
+FLOAT_ERROR_DELTA = 0.000_000_01
+numeric = Union[int, float]
--- test/test_quadratic_equation.py
+++ test/test_quadratic_equation.py
@@ -6,7 +6,7 @@ from small_tasks.quadratic_equation import solve_quadratic_equation
 class EquationTestCase(unittest.TestCase):
     def test_solve_quadratic_equation_a_zero(self):
         with self.assertRaises(ValueError) as exception_context:
-            solve_quadratic_equation(0.000000001, 1, 1)
+            solve_quadratic_equation(0.000_000_001, 1, 1)
         self.assertEqual(str(exception_context.exception), "Zero parameter A in equation")
 
 
