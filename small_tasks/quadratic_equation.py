f453b4ac030fcd8de5c0c29cc5364dd5671a5a04
Yuriy Shevchuk <33032673+yuriyShevchuk@users.noreply.github.com>
2022-12-27 20:58:12 +0300

    feat: all tests are completed
    * refactor: renamed test
    
    * feat: added check for inf and nan parameters

--- small_tasks/quadratic_equation.py
+++ small_tasks/quadratic_equation.py
@@ -1,15 +1,8 @@
 from const import *
 
 
-def numbers_equal(compare_what: numeric, compare_with: numeric) -> bool:
-    if abs(compare_what - compare_with) < FLOAT_ERROR_DELTA:
-        return True
-    return False
-
-
 def solve_quadratic_equation(a: float, b: float, c: float) -> tuple:
-    if numbers_equal(a, 0):
-        raise ValueError(ZERO_A_PARAMETER_ERROR_MSG)
+    check_arguments_for_validity(a, b, c)
 
     discriminant = b ** 2 - 4 * a * c
     if numbers_equal(discriminant, 0):
@@ -19,6 +12,31 @@ def solve_quadratic_equation(a: float, b: float, c: float) -> tuple:
         return ()
 
     return (-b + discriminant ** 0.5) / 2 * a, (-b - discriminant ** 0.5) / 2 * a
+
+
+def check_arguments_for_validity(a, b, c):
+    if check_iterable_for_infinite_or_nan_member([a, b, c]):
+        raise ValueError(INFINITE_OR_NAN_PARAMETER_ERROR)
+
+    if numbers_equal(a, 0):
+        raise ValueError(ZERO_A_PARAMETER_ERROR_MSG)
+
+
+def numbers_equal(compare_what: numeric, compare_with: numeric) -> bool:
+    if abs(compare_what - compare_with) < FLOAT_ERROR_DELTA:
+        return True
+    return False
+
+
+def check_iterable_for_infinite_or_nan_member(iterable_to_check):
+    for value in iterable_to_check:
+        if value != value:
+            return True
+
+        if value == float('inf') or value == float('-inf'):
+            return True
+
+    return False
 
 
 if __name__ == '__main__':
